73c381ae83c35bd3ba2a5b259eef642a469d1260
Daavid Stein <steindaavid@gmail.com>
2024-08-09 06:08:58 -0400

    remove tabs on overlay.py

--- overlay.py
+++ overlay.py
@@ -5,12 +5,12 @@ import numpy as np
 
 
 
-irf_tab, iic_tab, se_tab, var_tab, entropy_tab = st.tabs(["Item Response Function", "Item Information", "Standard Error of Estimate", "Variance", "Entropy"])
+#irf_tab, iic_tab, se_tab, var_tab, entropy_tab = st.tabs(["Item Response Function", "Item Information", "Standard Error of Estimate", "Variance", "Entropy"])
 
 
 with st.sidebar:
     #theta = st.slider('theta', min_value=-8.0, max_value=8.0, value=0.0)
-    a = st.slider('a (Discrimination)', min_value=0.0, max_value=3.0, value=1.0)
+    a = st.slider('a (Discrimination)', min_value=0.0, max_value=3.0, value=2.0)
     b = st.slider('b (Difficulty)', min_value=-8.0, max_value=8.0, value=0.0)
     # with st.container() as ckbox_container:
     #     show_irf = st.checkbox("Item Response Function")
@@ -21,75 +21,44 @@ with st.sidebar:
 
 
 
-with irf_tab:
-    x = 5
-    theta = np.arange(-8.0,8.0,0.1)
-    fig, ax = plt.subplots()
-    plt.plot(theta,irf(ability=theta, diff =b, disc=a))
-    plt.title(f"Item Response Function (2PL)"
+x = np.arange(-4, 4, 0.1)
+fig, ax = plt.subplots()
 
-              f"\nDifficulty={b},Discrimination={a}")
-    plt.suptitle( rf'$P(\theta) = \dfrac{{1}}{{1 + e^{{-{a} \cdot (\theta - {b})}}}}$',y = 0.65, x=0.35)
-    plt.xlabel("θ (Ability)")
-    plt.ylabel("Probability")
-    st.pyplot(fig)
+y = iic(ability=x, disc=a, diff=b,scale=False)
+ax.plot(x, y, label="Item Information (IIF)")
+plt.title(f"2PL \nDifficulty={b},Discrimination={a}")
+plt.text(s=rf"$IIF(\theta) = {{{a}}}^2 \cdot P(\theta) \cdot (1 - P(\theta))$",y = 2.3, x=-4)
+plt.xlabel("θ (Ability)")
+#plt.ylabel("Information")
+# ax = plt.gca()
+# ax.set_ylim([0, 2.5])
+#st.pyplot(fig)
 
-with iic_tab:
+prob = irf(ability=x, disc=a, diff=b)
+y = prob*(1-prob)
+ax.plot(x, y, label="Item Response Variance")
+#plt.title(f"Item Response Variance (2PL) \nDifficulty={b},Discrimination={a}")
+plt.text(s=rf"$Var(Response(\theta)) =\cdot P(\theta) \cdot (1 - P(\theta))$", y=2.15, x=-4)
+plt.xlabel("θ (Ability)")
+#plt.ylabel("Variance")
+# ax = plt.gca()
+# ax.set_ylim([0, 0.3])
+#st.pyplot(fig)
 
-    x = np.arange(-4, 4, 0.1)
-    fig, ax = plt.subplots()
-    y = iic(ability=x, disc=a, diff=b,scale=False)
-    ax.plot(x, y)
-    plt.title(f"Item Information Function (2PL) \nDifficulty={b},Discrimination={a}")
-    plt.suptitle(rf"$IIC(\theta) = {{{a}}}^2 \cdot P(\theta) \cdot (1 - P(\theta))$",y = 0.85, x=0.32)
-    plt.xlabel("θ (Ability)")
-    plt.ylabel("Information")
-    ax = plt.gca()
-    ax.set_ylim([0, 2.5])
-    st.pyplot(fig)
-
-with se_tab:
-
-    x = np.arange(-4, 4, 0.1)
-    fig, ax = plt.subplots()
-    y = se_est(ability=x, disc=a, diff=b)
-    ax.plot(x, y)
-    plt.title(f"Standard Error of Estimate (2PL) \nDifficulty={b},Discrimination={a}")
-    plt.suptitle(rf'$SE_{{est}}(\theta) = \dfrac{{1}}{{\sqrt{{({a})^2 \cdot P(\theta) \cdot (1 - P(\theta))}}}}$',y = 0.23, x=0.5)
-    plt.xlabel("θ (Ability)")
-    plt.ylabel("Standard Error of Estimate")
-    ax = plt.gca()
-    ax.set_ylim([0, 5])
-    st.pyplot(fig)
-
-with var_tab:
-    x = np.arange(-4, 4, 0.1)
-    fig, ax = plt.subplots()
-    prob = irf(ability=x, disc=a, diff=b)
-    y = prob*(1-prob)
-    ax.plot(x, y)
-    plt.title(f"Item Response Variance (2PL) \nDifficulty={b},Discrimination={a}")
-    plt.suptitle(rf"$Var(Response(\theta)) =\cdot P(\theta) \cdot (1 - P(\theta))$", y=0.85, x=0.4)
-    plt.xlabel("θ (Ability)")
-    plt.ylabel("Variance")
-    ax = plt.gca()
-    ax.set_ylim([0, 0.3])
-    st.pyplot(fig)
-
-with entropy_tab:
-    #   a = 1
-    x = np.arange(-4, 4, 0.1)
-    fig, ax = plt.subplots()
-    prob = irf(ability=x, disc=a, diff=b)
-    entropy = -sum([prob*np.log2(prob)])
-    ax.plot(x, entropy)
-    plt.title(f"Item Entropy (2PL)\nDifficulty={b},Discrimination={a}")
-    # plt.suptitle(rf"$Var(Response(\theta)) =\cdot P(\theta) \cdot (1 - P(\theta))$", y=0.85, x=0.4)
-    plt.xlabel("θ (Ability)")
-    plt.ylabel("Entropy")
-    ax.set_ylim([0, 0.6])
-    ax = plt.gca()
-    st.pyplot(fig)
+#FIXME I think this is wrong. if the entropy is the sum over all abilities, then shouldn't it be constant
+# for a given item, and not vary with ability as in our graph?
+# I think maybe we want the marginal entropy - the item entropy at a given theta.
+entropy = -(prob*np.log2(prob) + (1-prob)*np.log2(1-prob))
+ax.plot(x, entropy,label="Item Entropy")
+#plt.title(f"Item Entropy (2PL)\nDifficulty={b},Discrimination={a}")
+plt.text(s=rf"$Entropy(\theta)= -[ P(\theta) \log_2(P(\theta)) + (1-P(\theta)) \log_2(1-P(\theta)) ]$", y=1.8, x=-4)
+plt.xlabel("θ (Ability)")
+#plt.ylabel("Entropy")
+ax.set_ylim([0, 2.5])
+#ax = plt.gca()
+plt.legend()
+plt.tight_layout()
+st.pyplot(fig)
 
 
 
